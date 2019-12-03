b90651b0868f6a488c3181e76873a5191e65f1b3
Phong <phongdoan.dev@gmail.com>
2019-12-02 20:34:44 -0800

    Complete Challenge 7

--- for-each/challenges-01.test.js
+++ for-each/challenges-01.test.js
@@ -141,7 +141,19 @@ Return the resulting output array.
 ------------------------------------------------------------------------------------------------ */
 
 const fizzbuzz = (arr) => {
-  // Solution code here...
+  let outputArr = [];
+  arr.forEach(function(numbers){
+    if (numbers % 3 === 0 && numbers % 5 === 0){
+      outputArr.push('Fizz Buzz');
+    } else if ( numbers % 5 === 0){
+      outputArr.push('Buzz');
+    } else if ( numbers % 3 === 0){
+      outputArr.push('Fizz');
+    } else {
+      outputArr.push(numbers);
+    }
+  })
+  return outputArr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -198,7 +210,7 @@ describe('Testing challenge 6', () => {
   });
 });
 
-xdescribe('Testing challenge 7', () => {
+describe('Testing challenge 7', () => {
   const inputs = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
 
   test('It should print out messages or numbers', () => {
