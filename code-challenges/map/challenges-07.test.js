5a6fd4a37bd0400c118fcb35307887c2b8e85640
Phong <phongdoan.dev@gmail.com>
2019-12-10 19:48:52 -0800

    completed #2

--- code-challenges/map/challenges-07.test.js
+++ code-challenges/map/challenges-07.test.js
@@ -26,6 +26,10 @@ Write a function named forEachTwoToThe that produces the same output as your for
 
 const forEachTwoToThe = (arr) => {
   // Solution code here...
+  arr.forEach((num, index, array) => {
+    array[index] = 2 ** num;
+  })
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -181,7 +185,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should return two raised to the power of the integer', () => {
     expect(forEachTwoToThe([0, 4, 5])).toStrictEqual([1, 16, 32]);
     expect(forEachTwoToThe([0, 4, 5]).length).toStrictEqual(3);
