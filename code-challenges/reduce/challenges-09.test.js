289b57a97962636a3711aa50ef92c79dbf49d218
Phong <phongdoan.dev@gmail.com>
2019-12-12 20:55:44 -0800

    #3 completed

--- code-challenges/reduce/challenges-09.test.js
+++ code-challenges/reduce/challenges-09.test.js
@@ -87,6 +87,7 @@ Note: You must use reduce for this challenge. You may not use the built-in .reve
 
 const reversedString = (str) => {
   // Solution code here...
+  return str.split('').reduce((firstChar, secChar) => secChar + firstChar);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -255,7 +256,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should return the string with the characters in reverse order', () => {
     expect(reversedString('Code 301')).toStrictEqual('103 edoC');
   });
