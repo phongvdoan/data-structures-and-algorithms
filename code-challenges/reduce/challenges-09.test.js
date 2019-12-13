9dadd83cd8bd3d9928adb6b323401bf82daa240c
Phong <phongdoan.dev@gmail.com>
2019-12-12 20:47:45 -0800

    #2 completed

--- code-challenges/reduce/challenges-09.test.js
+++ code-challenges/reduce/challenges-09.test.js
@@ -10,7 +10,7 @@ Note: You may not use the array's built-in length property.
 
 const countNumberOfElements = (arr) => {
   // Solution code here...
-  return arr.reduce((a,b) => a + 1, 0);
+  return arr.reduce(a => a + 1, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -71,6 +71,10 @@ let starWarsData = [{
 
 const returnNames = (arr) => {
   // Solution code here...
+  return arr.reduce((array, nextVal) => {
+    array.push(nextVal.name);
+    return array;
+  }, []);
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -244,7 +248,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should return an array continaing the names of the characters', () => {
     expect(returnNames(starWarsData)).toStrictEqual([ 'Luke Skywalker', 'C-3PO', 'R2-D2', 'Darth Vader', 'Leia Organa' ]);
     expect(returnNames(starWarsData).length).toStrictEqual(5);
