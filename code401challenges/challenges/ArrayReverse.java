7514c4623a28d4ff7d88515819f96cb9a681928b
Phong <phongdoan.dev@gmail.com>
2020-01-06 19:01:34 -0800

    completed challenge

---
+++ code401challenges/challenges/ArrayReverse.java
@@ -0,0 +1,21 @@
+public class ArrayReverse {
+  public static void main(String[] arg) {
+    int[] array1 = { 1, 2, 3, 4, 5, 6 };
+    reverseArray(array1);
+    for (int i = 0; i < array1.length; i++) {
+      System.out.println(array1[i]);
+    }
+
+  }
+
+  public static int[] reverseArray(int[] array) {
+    if (array.length > 1) {
+      for (int i = 0; i < array.length / 2; i++) {
+        int previousValue = array[array.length - i - 1];
+        array[array.length - i - 1] = array[i];
+        array[i] = previousValue;
+      }
+    }
+    return array;
+  }
+}
