ae892e81ef384aaf4a59ca2f8903be864513fcb9
Phong <phongdoan.dev@gmail.com>
2020-01-07 22:08:50 -0800

    separated tests

--- code401challenges/src/test/java/code401challenges/LibraryTest.java
+++ code401challenges/src/test/java/code401challenges/LibraryTest.java
@@ -12,17 +12,4 @@ public class LibraryTest {
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
 
-    @Test public void testInsertShiftArrayMethod() {
-        int[] testDataOne = new int[]{2, 4, 6, 8};
-        int[] testDataTwo = new int[]{4, 8, 15, 23, 42};
-
-
-        assertEquals("[2, 4, 5, 6, 8]", code401challenges.ArrayShift.insertShiftArray(testDataOne, 5));
-        assertEquals("[4, 8, 15, 16, 23, 42]", code401challenges.ArrayShift.insertShiftArray(testDataTwo, 16));
-    }
-
-    @Test public void testFailureShiftArray() {
-        int[] testDataThree = new int[] {1,2,3,4,5,6};
-        assertNotEquals("[1,2,3,4,5,6,7]", code401challenges.ArrayShift.insertShiftArray(testDataThree, 7));
-    }
 }
