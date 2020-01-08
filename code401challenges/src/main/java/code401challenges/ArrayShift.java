277acc38c33d61ff976a769891d66ba8be188243
Phong <phongdoan.dev@gmail.com>
2020-01-07 20:18:45 -0800

    completed code challenge with tests

---
+++ code401challenges/src/main/java/code401challenges/ArrayShift.java
@@ -0,0 +1,23 @@
+package code401challenges;
+
+import java.util.Arrays;
+
+public class ArrayShift {
+    public static void main(String[] args) {
+
+    }
+
+    public static String insertShiftArray(int[] initialArray, int num) {
+        int[] newArr = new int[initialArray.length + 1];
+        int halfOfLengthOfNewArr = newArr.length / 2;
+        for(int i = 0; i < newArr.length; i++)
+            if (i < halfOfLengthOfNewArr){
+                newArr[i] = initialArray[i];
+            } else if (i == halfOfLengthOfNewArr){
+                newArr[i] = num;
+            } else {
+                newArr[i] = initialArray[i-1];
+            }
+        return Arrays.toString(newArr);
+    }
+}
