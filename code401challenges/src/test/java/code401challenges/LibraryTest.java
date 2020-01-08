f4b427d77d807933085fe234f4d829cebf3ad55f
Phong <phongdoan.dev@gmail.com>
2020-01-07 20:50:16 -0800

    added failure test

--- code401challenges/src/test/java/code401challenges/LibraryTest.java
+++ code401challenges/src/test/java/code401challenges/LibraryTest.java
@@ -13,10 +13,16 @@ public class LibraryTest {
     }
 
     @Test public void testInsertShiftArrayMethod() {
-        int[] testDataOne = new int[] {2,4,6,8};
-        int[] testDataTwo = new int[] {4,8,15,23,42};
+        int[] testDataOne = new int[]{2, 4, 6, 8};
+        int[] testDataTwo = new int[]{4, 8, 15, 23, 42};
+
 
         assertEquals("[2, 4, 5, 6, 8]", code401challenges.ArrayShift.insertShiftArray(testDataOne, 5));
         assertEquals("[4, 8, 15, 16, 23, 42]", code401challenges.ArrayShift.insertShiftArray(testDataTwo, 16));
+    }
+
+    @Test public void testFailureShiftArray() {
+        int[] testDataThree = new int[] {1,2,3,4,5,6};
+        assertNotEquals("[1,2,3,4,5,6,7]", code401challenges.ArrayShift.insertShiftArray(testDataThree, 7));
     }
 }
