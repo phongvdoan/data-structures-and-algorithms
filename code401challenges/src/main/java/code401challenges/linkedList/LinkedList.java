285135df86d12ee65419b559e0ece7ea4d5bcd06
Phong <phongdoan.dev@gmail.com>
2020-01-13 13:29:42 -0800

    fixed

--- code401challenges/src/main/java/code401challenges/linkedList/LinkedList.java
+++ code401challenges/src/main/java/code401challenges/linkedList/LinkedList.java
@@ -1,6 +1,8 @@
 //https://stackoverflow.com/questions/12631333/initializing-singly-linked-list-java
 package code401challenges.linkedList;
 
+import org.apache.commons.math3.exception.NullArgumentException;
+
 public class LinkedList {
 
     public Node head;
@@ -11,11 +13,11 @@ public class LinkedList {
 
     //https://www.youtube.com/watch?v=SMIq13-FZSE
     // this adds to the beginning versus the video shows to add to the end.
-    public void insert(Integer value) {
-            this.head = new Node(value, this.head);
+    public void insert(Integer value) throws NullArgumentException {
+            head = new Node(value, head);
     }
 
-    public boolean includes(Integer value) {
+    public boolean includes(Integer value) throws NullArgumentException {
         Node currentNode = this.head;
         while(currentNode != null) {
             if (currentNode.value == value) {
@@ -26,7 +28,7 @@ public class LinkedList {
         return false;
     }
 
-    public String toString() {
+    public String toString() throws NullPointerException {
         Node currentNode = this.head;
         String result = "The current values are: ";
         while(currentNode != null){
